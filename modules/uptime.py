d4bfe1f020bbe6e7a701fb7a7d844ba0bf86ccc3
Ville Ranki <ville.ranki@iki.fi>
2019-12-09 20:44:45 +0200

    Added uptime module

---
+++ modules/uptime.py
@@ -0,0 +1,14 @@
+import time
+
+class MatrixModule:
+    def matrix_start(self, bot):
+        self.starttime = time.time()
+
+    async def matrix_message(self, bot, room, event):
+        await bot.send_text(room, 'Uptime: ' + str(int(time.time() - self.starttime)) + ' seconds.')
+
+    def matrix_stop(self, bot):
+        pass
+
+    def help(self):
+        return('Tells how many seconds the bot has been up.')
