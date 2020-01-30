041ac0e9767966a7cedb6f017f2647f6b1bc3eaa
plocki <plocki@gmx.net>
2020-01-30 21:56:03 +0100

    weather.py für ansiweather

---
+++ modules/weather.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+
+class MatrixModule:
+    async def matrix_message(self, bot, room, event):
+        args = event.body.split()
+        args.pop(0)
+        encoding="utf-8"
+
+        # get weather from ansiweather
+        result = subprocess.check_output(["ansiweather","-a false","-l", ' '.join(args)])
+
+        await bot.send_text(room, result.decode(encoding))
+
+    def help(self):
+        return('How\'s the weather?')
