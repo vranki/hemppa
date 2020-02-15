86a89c7d448acb975fa98519b5ebb0e9bd3f5bae
Frank Becker <frank@calmdevelopment.de>
2020-02-15 01:14:37 +0100

    devlug dispatch module
    dispatch messages from matterbridge formatted like
    
    [irc] <ancho> This is a message
    
    that is. only "This is a message" is passed to the bot
    command handler

---
+++ modules/devlugdispatch.py
@@ -0,0 +1,75 @@
+import re
+
+from nio import RoomMessageText
+
+from modules.common.module import BotModule
+
+
+class MatterMessage:
+    def __init__(self, protocol, user, message):
+        self.protocol = protocol
+        self.username = user
+        self.message = message
+
+
+class MatterBridgeParser:
+
+    def __init__(self):
+        self.reg = re.compile(r"\[(.*)\] <(.*)> (.*)")
+        self.match = None
+
+    def validate(self, message):
+        self.match = self.reg.match(message)
+        return self.match is not None
+
+    def parse(self, message):
+
+        if self.validate(message):
+            groups = self.match.group(1, 2, 3)
+            return MatterMessage(groups[0], groups[1], groups[2])
+        else:
+            return None
+
+
+class MatrixModule(BotModule):
+    """
+    Everytime a matterbridge message is seen, the original message is delegated to the bot for command processing
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.bot = None
+        self.parser = MatterBridgeParser()
+
+    async def matrix_message(self, bot, room, event):
+
+        # todo: add subcommand to add administrators
+        # todo: needs a mechanism to hook into admin check of the bot
+        pass
+
+    def help(self):
+        return "parses matterbridge messages and delegate the parsed message to the bot"
+
+    def matrix_start(self, bot):
+        self.bot = bot
+        super().matrix_start(bot)
+        bot.client.add_event_callback(self.dispatch_cb, RoomMessageText)
+
+    def matrix_stop(self, bot):
+        super().matrix_stop(bot)
+        bot.remove_callback(self.dispatch_cb)
+
+    async def dispatch_cb(self, room, event):
+        # no content at all?
+        if len(event.body) < 1:
+            return
+
+        matter_message = self.parser.parse(event.body)
+
+        if matter_message is None:
+            return
+
+        self.logger.info(f"room: {room.name} - dispatch matterbridge message to bot")
+
+        # dispatch. changing the body of the event triggers message_cb of the bot
+        event.body = matter_message.message
