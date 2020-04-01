664dd66dd3a7ac8417131a82df793f1ae7accb6c
Dylan Hackworth <dhpf@pm.me>
2020-03-31 23:54:03 -0500

    modules/room: Added servers command
    Signed-off-by: Dylan Hackworth <dhpf@pm.me>

--- modules/room.py
+++ modules/room.py
@@ -13,3 +13,23 @@ class MatrixModule(BotModule):
         args = event.body.split()
         args.pop(0)
 
+        if args[0] == 'servers':
+            await MatrixModule.servers_in_room(bot, room)
+
+    @staticmethod
+    async def servers_in_room(bot, room: nio.MatrixRoom):
+        servers_in_room = "Servers in room:\n"
+        count = 0
+        response = await bot.client.joined_members(room.room_id)
+
+        if isinstance(response, nio.JoinedMembersError):
+            raise response
+        else:
+            for member in response.members:
+                assert isinstance(member, nio.RoomMember)
+                server = member.user_id.split(':')[1]
+                if server not in servers_in_room:
+                    count += 1
+                    servers_in_room += f" - {server}\n"
+            servers_in_room = f"({count}) {servers_in_room}"
+            await bot.send_text(room, servers_in_room)
