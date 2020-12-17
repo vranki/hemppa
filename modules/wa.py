e55af556aedc2d1b5e1d58bed1e8edabf84f0b43
Ville Ranki <ville.ranki@iki.fi>
2020-12-17 22:07:11 +0200

    Wolfram Alpha works again. Fixes #114.

--- modules/wa.py
+++ modules/wa.py
@@ -28,12 +28,15 @@ class MatrixModule(BotModule):
             answer = query + ': '
             try:
                 res = client.query(query)
-                for pod in res.results:
-                    for sub in pod.subpods:
-                        print(sub)
-                        answer += str(sub.plaintext) + "\n"
+                result = "?SYNTAX ERROR"
+                if res['@success']=='true':
+                    pod0=res['pod'][0]['subpod']['plaintext']
+                    pod1=res['pod'][1]
+                    if (('definition' in pod1['@title'].lower()) or ('result' in  pod1['@title'].lower()) or (pod1.get('@primary','false') == 'true')):
+                        result = pod1['subpod']['plaintext']
+                answer += result + "\n"
             except Exception as exc:
-                answer = str(exc)
+                answer = "Wolfram Alpha has technical difficulty: " + str(exc)
 
             await bot.send_text(room, answer)
         else:
