b0996037305f0c57b95f28c30cd2c1478dcbe6b2
Ville Ranki <ville.ranki@iki.fi>
2025-02-01 22:24:18 +0200

    TAF fixed

--- modules/taf.py
+++ modules/taf.py
@@ -1,4 +1,4 @@
-import urllib.request
+import requests
 
 from modules.common.module import BotModule
 
@@ -7,13 +7,11 @@ class MatrixModule(BotModule):
     async def matrix_message(self, bot, room, event):
         args = event.body.split()
         if len(args) == 2:
-            icao = args[1]
-            taf_url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=csv&hoursBeforeNow=3&timeType=issue&mostRecent=true&stationString=" + icao.upper()
-            response = urllib.request.urlopen(taf_url)
-            lines = response.readlines()
-            if len(lines) > 6:
-                taf = lines[6].decode("utf-8").split(',')[0]
-                await bot.send_text(room, taf.strip())
+            icao = args[1].upper()
+            taf_url = "https://aviationweather.gov/api/data/taf?ids=" + icao
+            response = requests.get(taf_url)
+            if len(response.text) > 0:
+                await bot.send_text(room, response.text)
             else:
                 await bot.send_text(room, 'Cannot find taf for ' + icao)
         else:
